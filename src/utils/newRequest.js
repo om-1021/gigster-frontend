efcf14b59f0ec4c51530a0359a08bf144a854c92
omdubey <omdubey1021@gmail.com>
2023-08-27 15:27:24 +0530

    fixed accessing accessToken before initalization

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../helper";
-import { config, parse } from "dotenv";
+import { config } from "dotenv";
 const url = BASE_URL;
 
 const newRequest = axios.create({
@@ -11,9 +11,11 @@ const newRequest = axios.create({
 newRequest.interceptors.request.use(
   (config) => {
     const user = localStorage.getItem("currentUser");
-    const parsedUser = JSON.parse(user);
-    const token = parsedUser.accessToken;
-    if (token) {
+    // const token = localStorage.getItem("acessToken");
+    if (user) {
+      // const user = localStorage.getItem("currentUser");
+      const parsedUser = JSON.parse(user);
+      const token = parsedUser.accessToken;
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
